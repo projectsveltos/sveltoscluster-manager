1c7aa8d3ddecbf1ec58dd3cd7b48e1be77c0b1f7
mgianluc <mgianluca@cisco.com>
2023-06-29 16:57:12 +0200

    Advance pins
    - k8s v1.27.2
    - controller-runtime v0.15.0
    - cluster-api v1.5.0-beta.0

--- hack/tools/tools.go
+++ hack/tools/tools.go
@@ -29,5 +29,4 @@ import (
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kind"
-	_ "sigs.k8s.io/kustomize/kustomize/v4"
 )
